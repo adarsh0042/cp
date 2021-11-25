822b76541164dcb75e0e7eb66edf6b850f25edec
Adarsh Kumar <kumar.adarsh0042@gmail.com>
2021-11-26 01:07:12 +0530

    26 nov

---
+++ cf_div3_756.cpp
@@ -0,0 +1,53 @@
+// A. Make Even
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <string>
+using namespace std;
+
+void solve() {
+    int a, b;
+    cin>>a>>b;
+    int ans=min((a+b)/4, min(a, b));
+    cout<<ans<<endl;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
+// B. Team Composition: Programmers and Mathematicians
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <string>
+using namespace std;
+
+void solve() {
+    int a, b;
+    cin>>a>>b;
+    int ans=min((a+b)/4, min(a, b));
+    cout<<ans<<endl;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
+// 
